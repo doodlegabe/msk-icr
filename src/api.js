fff6809fe4dadb87efce6802602b3b5cb54b672d
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-06-25 15:46:08 -0400

    no idea or hardened port

--- src/api.js
+++ src/api.js
@@ -2,10 +2,11 @@ require('dotenv').config('../.env');
 import express from 'express'
 import bodyParser from 'body-parser';
 import routes from './routes/index';
-const PORT = process.env.API_PORT;
-const HOST = process.env.API_HOST;
+
 
 const app = express();
+const port = process.env.PORT || 5050;
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -14,8 +15,9 @@ app.use(express.static('public'));
  * Routes
  */
 app.get('/providers', routes.provider.getProviders);
-app.listen(PORT, () => {
-  console.log('running on ' + HOST + ':' + PORT);
+
+app.listen(port, () => {
+  console.log('listening on ' + port);
 });
 
 
