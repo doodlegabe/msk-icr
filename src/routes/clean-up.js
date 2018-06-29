890f168eff4dbaedec84a6847d61bae35beff88d
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-06-29 13:41:50 -0400

    trying gitkeep

--- src/routes/clean-up.js
+++ src/routes/clean-up.js
@@ -4,9 +4,6 @@ import fs from 'fs'
 
 exports.doClean = function (req, res) {
   let fileToDelete = req.body.filePath;
-  console.log('------');
-  console.log(fileToDelete);
-
   fs.unlink(fileToDelete, (err) => {
     if (err) {
       res.status(500).send({error: err});
